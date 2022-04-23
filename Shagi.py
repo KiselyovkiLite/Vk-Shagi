92b2317c8250ec6507c789d747c9a7d0271c41e3
KiselyovkiLite <48594995+KiselyovkiLite@users.noreply.github.com>
2022-04-23 09:47:19 +0300

    Update Shagi.py

--- Shagi.py
+++ Shagi.py
@@ -1,8 +1,28 @@
-from urllib import request
+import requests
 from datetime import datetime
+from config import token
+print('Взлом вк шагов....начался')
 steps = 80000
 distance = 50000
-date = datetime.today().strftime('%Y-%m-%d')
-access_token = 'cf916985f2217d657632794548337fb588f6389691874a87d736dec92012955744fe81ed59e3ed4d834bf'
+if token == '':
+    print('Введите токен от страницы')
+    token=input()
+
+new_date=input('Сегодняшняя дата?(Y/N)')
+if new_date.lower() == 'n':
+    date = input('Формат YYYY-MM-DD')
+elif new_date.lower() == 'y':
+    date = datetime.today().strftime('%Y-%m-%d')
+else:
+    print('Ладно похуй тебя хуй поймешь давай сегоднящнуюю')
+    date = datetime.today().strftime('%Y-%m-%d')
 user_agent = 'VKAndroidApp/7.7-10445 (Android 11; SDK 30; arm64-v8a; Xiaomi M2003J15SC; ru; 2340x1080)'
-print(request.urlopen(request.Request('https://api.vk.com/method/vkRun.setSteps?steps='+str(steps)+'&distance='+str(distance)+'&date='+date+'&access_token='+access_token+'&v=5.131', headers={'User-Agent': user_agent})).read().decode('utf-8'))
+
+req = requests.get(f'https://api.vk.com/method/vkRun.setSteps?steps={steps}&distance={distance}&date={date}'
+                   f'&access_token={token}&v=5.131',headers={'User-Agent': user_agent})
+
+if req.text == '{"response":{"steps":80000,"distance":50000}}':
+    print("Взлом успешный иди проверяй шаги")
+else:
+    print('какая та ошибка короче чота ты накосячил')
+    print(req.text)
